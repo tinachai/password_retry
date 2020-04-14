89c801331fd5bd0cbb8cc8d0b78b1feb7db35a10
Tina Chai <thh258369@gmail.com>
2020-04-14 18:38:58 +0200

    updated version2

--- password_entry.py
+++ password_entry.py
@@ -1,10 +1,14 @@
 password = 'a123456'
 x = 3
-while x > 0: #當x大於0的時候就不用再迴圏了
+while x > 0: 
+	x = x - 1
 	guess = input('請輸入密碼: ')
 	if guess == password:
 		print('登入成功!')
 		break
 	else:
-		x = x-1
-		print('密碼錯誤!還有',x,'次機會' )
+		print('密碼錯誤!')
+		if x > 0:
+			print('還有',x,'機會')
+		else:
+			print('沒有機會了!')
